a1372bfeb768c462bd2204a32ef07ce42fb5e2bb
Géraldine MUSAT <geraldine.musat@hotmail.fr>
2018-12-19 17:53:10 +0100

    Debut application
    Juste les liens et la structure de base

--- my-app-vue-cli1/src/main.js
+++ my-app-vue-cli1/src/main.js
@@ -1,7 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import Restaurants from './components/Restaurants.vue';
+import Add from './components/AddRestaurants.vue';
+import Delete from './components/DeleteRestaurants.vue';
+
+// Définition des routes
+Vue.use(VueRouter);
+
+const routes = [
+	{path:'/restaurants', component: Restaurants},
+	{path:'/add', component: Add},
+	{path:'/delete', component: Delete},
+]
+
+const router = new VueRouter({
+	routes: routes,
+	mode:'history'
+})
 
 new Vue({
   el: '#app',
+  router:router,
   render: h => h(App)
 })
